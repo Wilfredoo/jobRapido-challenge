a0576ba80b9cc2d52d0bf95a2ce051e58e812ec3
Wilfredo <casas.farach@gmail.com>
2020-10-23 13:24:48 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import jobs from "./jobs.json";
-import moment from "moment";
 
 function App() {
   return (
     <div className="App">
-      <h1>JobDirecto Challenge</h1>
+      <h1>JobRapido Challenge</h1>
       <h2>Jobs in New York</h2>
       <div className="allPosts">
-        {Object.entries(jobs).map(([key, value]) => {
-          return (
-            <div className="jobPost" key={value._id}>
-              <p className="text employer">{value.employer}</p>
-              <p className="text jobType">{value.jobType}</p>
-              <p className="text area">{value.area}</p>
-              <p className="text createdAt">{moment(value.createdAt).fromNow()}</p>
-            </div>
-          );
-        })}
+      
       </div>
     </div>
   );
